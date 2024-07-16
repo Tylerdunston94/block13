917e3b80c00ac37bd3c34f098e8171ecb502da2c
Tyler Dunston <tylerdunston@Tylers-MacBook-Air.local>
2024-07-16 14:56:44 -0400

    second push

--- function.js
+++ function.js
@@ -1,82 +1,74 @@
-// Complete the following functions to make our program work!
-
 /**
- * Takes a F number and returns the number to C
- * @param {string} fahren temperature in degrees F
- * @returns {number} the number of degrees C
+ * Takes a Fahrenheit temperature and returns the temperature in Celsius
+ * @param {string} fahren temperature in degrees Fahrenheit
+ * @returns {number} the temperature in degrees Celsius
  */
-function convertToCelsius(fahren) {}
+function convertToCelcius(fahren) {
+  let celsius = (fahren - 32) * (5/9);
+  return celsius;
+}
 
 /**
- * Takes both numbers (F, C) and display a message with
- * both numbers and how that temp makes you feel using
- * this chart
+ * Takes both numbers (F, C) and displays a message with
+ * both numbers and how that temperature makes you feel using this chart:
  *
  *   Temp      Feels
  * ---------|----------
- *   < 20 |  "very cold"
- *   < 45   |  "cold"
- *   < 80   |  "warm"
+ *   < 32   |  "very cold"
+ *   < 64   |  "cold"
+ *   < 86   |  "warm"
  *   < 100  |  "hot"
  *
- * @param {number} fahren
- * @param {number} celc
+ * @param {number} fahren Fahrenheit temperature
+ * @param {number} celc Celsius temperature
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  let message = `The temperature in Fahrenheit is ${fahren} and in Celsius is ${celc}. It feels `;
+  
+  if (fahren < 32) {
+    message += `"very cold"`;
+  } else if (fahren < 64) {
+    message += `"cold"`;
+  } else if (fahren < 86) {
+    message += `"warm"`;
+  } else {
+    message += `"hot"`;
+  }
+
+  return message;
+}
 
 /**
- * Takes a number and returns a random integer from 0 to the limit
- * @param {number} limit
- * @returns {number} a number between 0 and the int passed in
+ * Takes a number and returns a random integer between 0 and the limit
+ * @param {number} limit maximum limit to generate a random number
+ * @returns {number} a random integer between 0 and the limit
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.floor(Math.random() * (limit + 1));
+}
 
 // -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
 
-let fahren = prompt(
-  "enter a number, we will convert that number from fahrenheit to celcius"
-);
-let celc = convertToCelsius(fahren);
+let fahren = parseInt(prompt(
+  "Enter a number, we will convert that number from Fahrenheit to Celsius"
+));
+let celc = convertToCelcius(fahren);
 let output = createMessage(fahren, celc);
 console.log(output);
 
-fahren = prompt(
-  "enter a number, we will convert that number from fahrenheit to celcius"
-);
-celc = convertToCelsius(fahren);
+fahren = parseInt(prompt(
+  "Let's try that again. Enter a number, we will convert that number from Fahrenheit to Celsius"
+));
+celc = convertToCelcius(fahren);
 output = createMessage(fahren, celc);
 console.log(output);
 
 fahren = rand(110);
-celc = convertToCelsius(fahren);
+celc = convertToCelcius(fahren);
 output = createMessage(fahren, celc);
 console.log(output);
 
 fahren = rand(110);
-celc = convertToCelsius(fahren);
+celc = convertToCelcius(fahren);
 output = createMessage(fahren, celc);
 console.log(output);
-
-/* Block 13 Temperature Converter */
-
-function convertToCelsius (fahren) {
-  return (fahren - 20) * 5/9;
-}
- 
-function createMessage (fahren, celc) {
-  let message = "the temperature is ${fahren" 
-  if (celc < 20) {
-    console.log("very cold");
-  } else if (celc < 45) {
-    console.log("cold");
-  } else if (celc < 80) {
-    console.log("warm");
-  } else if (celc < 100 ) {
-    console.log("hot");
-  }
-  return message;
-}
-  function rand(limit) {
-    return Math.floor(Math.random() * limit);
-  }
-
